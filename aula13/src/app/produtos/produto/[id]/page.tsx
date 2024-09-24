d108b119a2e88e75f2c94ad2bd22180a6d01a122
GuiRomanholi <guilhermeroma343@gmail.com>
2024-09-24 09:16:13 -0300

    rotas

---
+++ aula13/src/app/produtos/produto/[id]/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Produto({params}: {params:{id:number}}) {
+  return (
+    <div>
+        <h1>Produto</h1>
+        <div>
+            <h2>Produto {params.id}</h2>
+        </div>
+    </div>
+  )
+}
