176fa26484a41b0dca3fe8b2fd8d7ebdeca0bff4
LuizHNR <luizhneri12@gmail.com>
2024-09-24 09:45:05 -0300

    atributo layout na tag image: responsive, fill, intrinsic

--- aula13/src/app/not-found.tsx
+++ aula13/src/app/not-found.tsx
@@ -12,7 +12,7 @@ export default function NotFound() {
 
         <Image src="/img/404.jpg" alt="pagina de erro" width={400} height={400} />
 
-        <Image src="https://lojaintegrada.com.br/hub/wp-content/uploads/2023/05/erro-404-4.webp" alt="Erro 404" width={400} height={400} />
+        <Image layout="responsive" src="https://lojaintegrada.com.br/hub/wp-content/uploads/2023/05/erro-404-4.webp" alt="Erro 404" width={400} height={400} />
 
     </div>
   )
